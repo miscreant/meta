9b6798ca43b4f8c12e88291a8a55195391240049
Dmitry Chestnykh <dmitry@codingrobots.com>
2017-04-24 01:39:44 +0200

    siv: add package

---
+++ siv.test.ts
@@ -0,0 +1,143 @@
+// Copyright (C) 2017 Dmitry Chestnykh
+// MIT License. See LICENSE file for details.
+
+import { SIV } from "./siv";
+import { AES } from "@stablelib/aes";
+import { encode, decode } from "@stablelib/hex";
+
+// tslint:disable
+const vectors = [
+    // A.1.  Deterministic Authenticated Encryption Example
+    {
+        key: "fffefdfc fbfaf9f8 f7f6f5f4 f3f2f1f0 f0f1f2f3 f4f5f6f7 f8f9fafb fcfdfeff",
+        ad: ["10111213 14151617 18191a1b 1c1d1e1f 20212223 24252627"],
+        plaintext: "11223344 55667788 99aabbcc ddee",
+        output: "85632d07 c6e8f37f 950acd32 0a2ecc93 40c02b96 90c4dc04 daef7f6a fe5c",
+    },
+    //A.2.  Nonce-Based Authenticated Encryption Example
+    {
+        key: "7f7e7d7c 7b7a7978 77767574 73727170 40414243 44454647 48494a4b 4c4d4e4f",
+        ad: [
+            "00112233 44556677 8899aabb ccddeeff deaddada deaddada ffeeddcc bbaa9988 77665544 33221100", //AD1
+            "10203040 50607080 90a0",                                                                    //AD2
+            "09f91102 9d74e35b d84156c5 635688c0",                                                       // nonce
+        ],
+        plaintext: "74686973 20697320 736f6d65 20706c61 696e7465 78742074 6f20656e 63727970 74207573 696e6720 5349562d 414553",
+        output: "7bdb6e3b 432667eb 06f4d14b ff2fbd0f cb900f2f ddbe4043 26601965 c889bf17 dba77ceb 094fa663 b7a3f748 ba8af829 ea64ad54 4a272e9c 485b62a3 fd5c0d",
+    },
+    {
+        key: "fffefdfc fbfaf9f8 f7f6f5f4 f3f2f1f0 f0f1f2f3 f4f5f6f7 f8f9fafb fcfdfeff",
+        ad: [],
+        plaintext: "",
+        output: "f2007a5beb2b8900c588a7adf599f172",
+    },
+    // http://csrc.nist.gov/groups/ST/toolkit/BCM/documents/proposedmodes/siv/siv-test-vectors.txt
+    // TEST CASE #1
+    // 192 bit subkeys
+    {
+        key: "fffefdfc fbfaf9f8 f7f6f5f4 f3f2f1f0 6f6e6d6c 6b6a6968 f0f1f2f3 f4f5f6f7 f8f9fafb fcfdfeff 00010203 04050607",
+        ad: ["10111213 14151617 18191a1b 1c1d1e1f 20212223 24252627"],
+        plaintext: "11223344 55667788 99aabbcc ddee",
+        output: "02347811 daa8b274 91f24448 932775a6 2af34a06 ac0016e8 ac284a55 14f6",
+    },
+    // 256 bit subkeys
+    {
+        key: "fffefdfc fbfaf9f8 f7f6f5f4 f3f2f1f0 6f6e6d6c 6b6a6968 67666564 63626160 f0f1f2f3 f4f5f6f7 f8f9fafb fcfdfeff 00010203 04050607 08090a0b 0c0d0e0f",
+        ad: ["10111213 14151617 18191a1b 1c1d1e1f 20212223 24252627"],
+        plaintext: "11223344 55667788 99aabbcc ddee",
+        output: "f125274c 598065cf c26b0e71 57502908 8b035217 e380cac8 919ee800 c126",
+    },
+    // TEST CASE #2
+    // 192 bit subkeys
+    {
+        key: "7f7e7d7c 7b7a7978 77767574 73727170 6f6e6d6c 6b6a6968 40414243 44454647 48494a4b 4c4d4e4f 50515253 54555657",
+        ad: [
+            "00112233 44556677 8899aabb ccddeeff deaddada deaddada ffeeddcc bbaa9988 77665544 33221100",
+            "10203040 50607080 90a0",
+            "09f91102 9d74e35b d84156c5 635688c0",
+        ],
+        plaintext: "74686973 20697320 736f6d65 20706c61 696e7465 78742074 6f20656e 63727970 74207573 696e6720 5349562d 414553",
+        output: "de40aa1e 7180d519 cb14308e a7f77586 da09877c 510f2965 1f42311a b728e956 09e7de29 94bdf80b b99bfaac e31c4ec0 d15ba650 9f53f36a d725dcab c9e2a7",
+    },
+    // 256 bit subkeys
+    {
+        key: "7f7e7d7c 7b7a7978 77767574 73727170 6f6e6d6c 6b6a6968 67666564 63626160 40414243 44454647 48494a4b 4c4d4e4f 50515253 54555657 58595a5b 5b5d5e5f",
+        ad: [
+            "00112233 44556677 8899aabb ccddeeff deaddada deaddada ffeeddcc bbaa9988 77665544 33221100",
+            "10203040 50607080 90a0",
+            "09f91102 9d74e35b d84156c5 635688c0",
+        ],
+        plaintext: "74686973 20697320 736f6d65 20706c61 696e7465 78742074 6f20656e 63727970 74207573 696e6720 5349562d 414553",
+        output: "85b81673 10038db7 dc4692c0 281ca358 68181b27 62f3c24f 2efa5fb8 0cb14351 6ce6c434 b898a6fd 8eb98a41 8842f51f 66fc67de 43ac185a 66dd7247 5bbb08",
+    }
+];
+// tslint:enable
+
+describe("SIV (AES)", () => {
+    const dec = (s: string) => decode(s.replace(/ /g, ""));
+
+    it("should correctly seal and open", () => {
+        vectors.forEach(v => {
+            const key = dec(v.key);
+            const ad = v.ad.map(dec);
+            const plaintext = dec(v.plaintext);
+            const output = dec(v.output);
+
+            const siv = new SIV(AES, key);
+            const sealed = siv.seal(ad, plaintext);
+            expect(encode(sealed)).toEqual(encode(output));
+            const unsealed = siv.open(ad, sealed);
+            expect(unsealed).not.toBeNull();
+            if (unsealed) {
+                expect(encode(unsealed)).toEqual(encode(plaintext));
+            }
+            expect(() => siv.clean()).not.toThrow();
+        });
+    });
+
+    it("should not open with incorrect key", () => {
+        vectors.forEach(v => {
+            const badKey = dec(v.key);
+            badKey[0] ^= badKey[0];
+            badKey[2] ^= badKey[2];
+            badKey[3] ^= badKey[8];
+            const ad = v.ad.map(dec);
+            const output = dec(v.output);
+
+            const siv = new SIV(AES, badKey);
+            const unsealed = siv.open(ad, output);
+            expect(unsealed).toBeNull();
+        });
+    });
+
+
+    it("should not open with incorrect associated data", () => {
+        vectors.forEach(v => {
+            const key = dec(v.key);
+            const badAd = v.ad.map(dec);
+            badAd.push(new Uint8Array(1));
+            const output = dec(v.output);
+
+            const siv = new SIV(AES, key);
+            const unsealed = siv.open(badAd, output);
+            expect(unsealed).toBeNull();
+        });
+    });
+
+    it("should not open with incorrect ciphertext", () => {
+        vectors.forEach(v => {
+            const key = dec(v.key);
+            const ad = v.ad.map(dec);
+            const badOutput = dec(v.output);
+            badOutput[0] ^= badOutput[0];
+            badOutput[1] ^= badOutput[1];
+            badOutput[3] ^= badOutput[8];
+
+            const siv = new SIV(AES, key);
+            const unsealed = siv.open(ad, badOutput);
+            expect(unsealed).toBeNull();
+        });
+    });
+
+});
+
---
+++ siv.ts
@@ -0,0 +1,179 @@
+// Copyright (C) 2017 Dmitry Chestnykh
+// MIT License. See LICENSE file for details.
+
+import { BlockCipher, BlockCipherContructor } from "@stablelib/blockcipher";
+import { CMAC, dbl } from "@stablelib/cmac";
+import { CTR } from "@stablelib/ctr";
+import { wipe } from "@stablelib/wipe";
+import { equal } from "@stablelib/constant-time";
+
+/** Maximum number of associated data items */
+export const MAX_ASSOCIATED_DATA = 126;
+
+export class SIV {
+    _mac: CMAC;
+    _ctr: CTR | undefined;
+    _macCipher: BlockCipher;
+    _encCipher: BlockCipher;
+    _tmp1: Uint8Array;
+    _tmp2: Uint8Array;
+
+    tagLength: number;
+
+    constructor(cipher: BlockCipherContructor, key: Uint8Array) {
+        const macKey = key.subarray(0, key.length / 2 | 0);
+        const encKey = key.subarray(key.length / 2 | 0);
+
+        this._macCipher = new cipher(macKey);
+        this._encCipher = new cipher(encKey);
+        this._mac = new CMAC(this._macCipher);
+
+        if (this._mac.digestLength !== this._mac.blockSize) {
+            throw new Error("SIV: this implementation needs CMAC block size to equal tag length");
+        }
+        this.tagLength = this._mac.digestLength;
+
+        this._tmp1 = new Uint8Array(this._mac.digestLength);
+        this._tmp2 = new Uint8Array(this._mac.digestLength);
+    }
+
+    seal(associatedData: Uint8Array[], plaintext: Uint8Array, dst?: Uint8Array): Uint8Array {
+        if (associatedData.length > MAX_ASSOCIATED_DATA) {
+            throw new Error("SIV: too many associated data items");
+        }
+
+        // Allocate space for sealed ciphertext.
+        const resultLength = this.tagLength + plaintext.length;
+        let result;
+        if (dst) {
+            if (dst.length !== resultLength) {
+                throw new Error("SIV: incorrect destination length");
+            }
+            result = dst;
+        } else {
+            result = new Uint8Array(resultLength);
+        }
+
+        // Authenticate.
+        const iv = this._s2v(associatedData, plaintext);
+        result.set(iv);
+
+        // Encrypt.
+        zeroIVBits(iv);
+        this._streamXOR(iv, plaintext, result.subarray(iv.length));
+        return result;
+    }
+
+    open(associatedData: Uint8Array[], sealed: Uint8Array, dst?: Uint8Array): Uint8Array | null {
+        if (associatedData.length > MAX_ASSOCIATED_DATA) {
+            throw new Error("SIV: too many associated data items");
+        }
+        if (sealed.length < this.tagLength) {
+            return null;
+        }
+
+        // Allocate space for decrypted plaintext.
+        const resultLength = sealed.length - this.tagLength;
+        let result;
+        if (dst) {
+            if (dst.length !== resultLength) {
+                throw new Error("SIV: incorrect destination length");
+            }
+            result = dst;
+        } else {
+            result = new Uint8Array(resultLength);
+        }
+
+        // Decrypt.
+        const tag = sealed.subarray(0, this.tagLength);
+        const iv = this._tmp1;
+        iv.set(tag);
+        zeroIVBits(iv);
+        this._streamXOR(iv, sealed.subarray(this.tagLength), result);
+
+        // Authenticate.
+        const expectedTag = this._s2v(associatedData, result);
+        if (!equal(expectedTag, tag)) {
+            wipe(result);
+            return null;
+        }
+        return result;
+    }
+
+    _streamXOR(iv: Uint8Array, src: Uint8Array, dst: Uint8Array) {
+        if (!this._ctr) {
+            this._ctr = new CTR(this._encCipher, iv);
+        } else {
+            this._ctr.setCipher(this._encCipher, iv);
+        }
+        this._ctr.streamXOR(src, dst);
+        return dst;
+    }
+
+    _s2v(s: Uint8Array[], sn: Uint8Array): Uint8Array {
+        if (!s) {
+            s = [];
+        }
+
+        this._mac.reset();
+        wipe(this._tmp1);
+
+        // Note: the standalone S2V returns CMAC(1) if the number of passed
+        // vectors is zero, however in SIV contruction this case is never
+        // triggered, since we always pass plaintext as the last vector (even
+        // if it's zero-length), so we omit this case.
+        this._mac.update(this._tmp1);
+        this._mac.finish(this._tmp2);
+        this._mac.reset();
+
+        for (let i = 0; i < s.length; i++) {
+            this._mac.update(s[i]);
+            this._mac.finish(this._tmp1);
+            this._mac.reset();
+            dbl(this._tmp2, this._tmp2);
+            xor(this._tmp2, this._tmp1);
+        }
+
+        wipe(this._tmp1);
+
+        if (sn.length >= this._mac.blockSize) {
+            const n = sn.length - this._mac.blockSize;
+            this._tmp1.set(sn.subarray(n));
+            this._mac.update(sn.subarray(0, n));
+        } else {
+            this._tmp1.set(sn);
+            this._tmp1[sn.length] = 0x80;
+            dbl(this._tmp2, this._tmp2);
+        }
+        xor(this._tmp1, this._tmp2);
+        this._mac.update(this._tmp1);
+        this._mac.finish(this._tmp1);
+        return this._tmp1;
+    }
+
+    clean() {
+        wipe(this._tmp1);
+        wipe(this._tmp2);
+        if (this._ctr) {
+            this._ctr.clean();
+        }
+        this._mac.clean();
+        this._encCipher.clean();
+        this._macCipher.clean();
+        this.tagLength = 0;
+    }
+}
+
+function xor(a: Uint8Array, b: Uint8Array) {
+    for (let i = 0; i < b.length; i++) {
+        a[i] ^= b[i];
+    }
+}
+
+function zeroIVBits(iv: Uint8Array) {
+    // "We zero-out the top bit in each of the last two 32-bit words
+    // of the IV before assigning it to Ctr"
+    //  — http://web.cs.ucdavis.edu/~rogaway/papers/siv.pdf
+    iv[iv.length - 8] &= 0x7f;
+    iv[iv.length - 4] &= 0x7f;
+}
