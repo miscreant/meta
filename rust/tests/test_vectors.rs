3ab1857285fdfd779fb52f6816b7c954ff264cb7
Tony Arcieri <bascule@gmail.com>
2017-08-12 21:52:07 -0700

    vectors: AES-PMAC-SIV test vectors
    As AES-PMAC-SIV is effectively a novel construction, it has no existing test
    vectors to draw from.
    
    This set of test vectors was created by using the same inputs as the AES-SIV
    test vectors, and running them against the Rust AES-PMAC-SIV implementation.
    
    This implementation should be correct because:
    
    1) AES-PMAC has been tested against Rogaway's test vectors
    2) AES-SIV has been tested against several vectors
    3) AES-SIV is implemented as a generic construction, letting us swap within
       a known-good AES-SIV implementation a known-good PMAC implementation.
    
    Based on this, we use the Rust implementation as a reference, and generate test
    vectors from it in order to test correctness of the other language
    implementations.

--- rust/tests/test_vectors.rs
+++ rust/tests/test_vectors.rs
@@ -292,6 +292,75 @@ impl AesSivExample {
     }
 }
 
+/// AES-SIV test vectors
+// TODO: switch to the tjson crate (based on serde)
+#[derive(Debug)]
+pub struct AesPmacSivExample {
+    pub key: Vec<u8>,
+    pub ad: Vec<Vec<u8>>,
+    pub plaintext: Vec<u8>,
+    pub ciphertext: Vec<u8>,
+}
+
+impl AesPmacSivExample {
+    /// Load examples from aes_pmac_siv.tjson
+    pub fn load_all() -> Vec<Self> {
+        Self::load_from_file(Path::new("../vectors/aes_pmac_siv.tjson"))
+    }
+
+    /// Load examples from a file at the given path
+    pub fn load_from_file(path: &Path) -> Vec<Self> {
+        let mut file = File::open(&path).expect("valid aes_pmac_siv.tjson");
+        let mut tjson_string = String::new();
+        file.read_to_string(&mut tjson_string).expect(
+            "aes_pmac_siv.tjson read successfully",
+        );
+
+        let tjson: serde_json::Value =
+            serde_json::from_str(&tjson_string).expect("aes_pmac_siv.tjson parses successfully");
+        let examples = &tjson["examples:A<O>"].as_array().expect(
+            "aes_pmac_siv.tjson examples array",
+        );
+
+        examples
+            .into_iter()
+            .map(|ex| {
+                Self {
+                    key: HEXLOWER
+                        .decode(ex["key:d16"].as_str().expect("encoded example").as_bytes())
+                        .expect("hex encoded"),
+                    ad: ex["ad:A<d16>"]
+                        .as_array()
+                        .expect("encoded example")
+                        .iter()
+                        .map(|ex| {
+                            HEXLOWER
+                                .decode(ex.as_str().expect("encoded example").as_bytes())
+                                .expect("hex encoded")
+                        })
+                        .collect(),
+                    plaintext: HEXLOWER
+                        .decode(
+                            ex["plaintext:d16"]
+                                .as_str()
+                                .expect("encoded example")
+                                .as_bytes(),
+                        )
+                        .expect("hex encoded"),
+                    ciphertext: HEXLOWER
+                        .decode(
+                            ex["ciphertext:d16"]
+                                .as_str()
+                                .expect("encoded example")
+                                .as_bytes(),
+                        )
+                        .expect("hex encoded"),
+                }
+            })
+            .collect()
+    }
+}
+
 /// dbl() test vectors
 // TODO: switch to the tjson crate (based on serde)
 #[derive(Debug)]
