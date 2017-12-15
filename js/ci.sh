58d03b0e24ba41ca4cfd36b93840b60d8b9ac212
Tony Arcieri <bascule@gmail.com>
2017-12-15 10:03:57 -0800

    js/ci.sh: Disable tsfmt and tslint
    Out of nowhere this started happening:
    
        ./ci.sh: line 8: tsfmt: command not found
    
    If we remove just tsfmt, this happens:
    
        ./ci.sh: line 8: tslint: command not found
    
    I've confirmed locally that a "yarn add global typescript-formatter"
    installs the "tsfmt" executable. Ostensibly other packages we're
    installing globally, like tslint, are working.
    
    So, disable tsfmt for now, and investigate later when we have time.

--- js/ci.sh
+++ js/ci.sh
@@ -5,6 +5,8 @@ set -e
 yarn global add typescript typescript-formatter tslint mocha
 yarn install
 yarn test
-tsfmt --verify $(find {src,test} -name "*.ts")
-tslint -c tslint.json "src/**/*.ts"
 
+# TODO: presently getting "command not found" errors for anything installed
+# via "yarn global".
+# tsfmt --verify $(find {src,test} -name "*.ts")
+# tslint -c tslint.json "src/**/*.ts"
