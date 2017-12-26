570881c48b1066eb176a403c7c57b7fb56b12735
Nemanja Mijailovic <metalnem@mijailovic.net>
2017-12-26 15:08:59 +0100

    Add STREAM XML documentation

--- dotnet/Miscreant/StreamDecryptor.cs
+++ dotnet/Miscreant/StreamDecryptor.cs
@@ -3,6 +3,12 @@ using System.Security.Cryptography;
 
 namespace Miscreant
 {
+	/// <summary>
+	/// STREAM online authenticated encryption, defined in the paper
+	/// <see href="https://eprint.iacr.org/2015/189.pdf">
+	/// Online Authenticated-Encryption and its Nonce-Reuse Misuse-Resistance
+	/// </see>.
+	/// </summary>
 	public sealed class StreamDecryptor : IDisposable
 	{
 		private readonly AesSiv siv;
@@ -16,18 +22,41 @@ namespace Miscreant
 			this.nonce = new NonceEncoder(nonce);
 		}
 
+		/// <summary>
+		/// Initializes a new instance of the STREAM decryptor using the AES-CMAC-SIV algorithm.
+		/// </summary>
+		/// <param name="key">The secret key for decryption.</param>
+		/// <param name="nonce">The nonce for decryption.</param>
+		/// <returns>A STREAM decryptor instance.</returns>
 		public static StreamDecryptor CreateAesCmacSivDecryptor(byte[] key, byte[] nonce)
 		{
 			var siv = AesSiv.CreateAesCmacSiv(key);
 			return new StreamDecryptor(siv, nonce);
 		}
 
+		/// <summary>
+		/// Initializes a new instance of the STREAM decryptor using the AES-PMAC-SIV algorithm.
+		/// </summary>
+		/// <param name="key">The secret key for decryption.</param>
+		/// <param name="nonce">The nonce for decryption.</param>
+		/// <returns>A STREAM decryptor instance.</returns>
 		public static StreamDecryptor CreateAesPmacSivDecryptor(byte[] key, byte[] nonce)
 		{
 			var siv = AesSiv.CreateAesPmacSiv(key);
 			return new StreamDecryptor(siv, nonce);
 		}
 
+		/// <summary>
+		/// Open decrypts the next ciphertext in the STREAM, authenticates the
+		/// decrypted plaintext and the associated data and, if successful, returns
+		/// the result. In case of failed decryption, this method throws
+		/// <see cref="CryptographicException">.
+		/// </summary>
+		/// <param name="ciphertext">The ciphertext to decrypt.</param>
+		/// <param name="data">Associated data items to authenticate.</param>
+		/// <param name="last">True if this is the last block in the STREAM.</param>
+		/// <returns>The decrypted plaintext.</returns>
+		/// <exception cref="CryptographicException">Thrown when the ciphertext is invalid.</exception>
 		public byte[] Open(byte[] plaintext, byte[] data = null, bool last = false)
 		{
 			if (disposed)
--- dotnet/Miscreant/StreamEncryptor.cs
+++ dotnet/Miscreant/StreamEncryptor.cs
@@ -3,6 +3,12 @@ using System.Security.Cryptography;
 
 namespace Miscreant
 {
+	/// <summary>
+	/// STREAM online authenticated encryption, defined in the paper
+	/// <see href="https://eprint.iacr.org/2015/189.pdf">
+	/// Online Authenticated-Encryption and its Nonce-Reuse Misuse-Resistance
+	/// </see>.
+	/// </summary>
 	public sealed class StreamEncryptor : IDisposable
 	{
 		private readonly AesSiv siv;
@@ -16,18 +22,38 @@ namespace Miscreant
 			this.nonce = new NonceEncoder(nonce);
 		}
 
+		/// <summary>
+		/// Initializes a new instance of the STREAM encryptor using the AES-CMAC-SIV algorithm.
+		/// </summary>
+		/// <param name="key">The secret key for encryption.</param>
+		/// <param name="nonce">The nonce for encryption.</param>
+		/// <returns>A STREAM encryptor instance.</returns>
 		public static StreamEncryptor CreateAesCmacSivEncryptor(byte[] key, byte[] nonce)
 		{
 			var siv = AesSiv.CreateAesCmacSiv(key);
 			return new StreamEncryptor(siv, nonce);
 		}
 
+		/// <summary>
+		/// Initializes a new instance of the STREAM encryptor using the AES-PMAC-SIV algorithm.
+		/// </summary>
+		/// <param name="key">The secret key for encryption.</param>
+		/// <param name="nonce">The nonce for encryption.</param>
+		/// <returns>A STREAM encryptor instance.</returns>
 		public static StreamEncryptor CreateAesPmacSivEncryptor(byte[] key, byte[] nonce)
 		{
 			var siv = AesSiv.CreateAesPmacSiv(key);
 			return new StreamEncryptor(siv, nonce);
 		}
 
+		/// <summary>
+		/// Seal encrypts and authenticates the next message in the STREAM,
+		/// authenticates the associated data, and returns the result.
+		/// </summary>
+		/// <param name="plaintext">The plaintext to encrypt.</param>
+		/// <param name="data">Associated data items to authenticate.</param>
+		/// <param name="last">True if this is the last block in the STREAM.</param>
+		/// <returns>Concatenation of the authentication tag and the encrypted data.</returns>
 		public byte[] Seal(byte[] plaintext, byte[] data = null, bool last = false)
 		{
 			if (disposed)
